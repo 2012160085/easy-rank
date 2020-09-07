5f165d7c79144381d1cea21a38f6d33c24159784
2012160085 <70303094+2012160085@users.noreply.github.com>
2020-09-07 13:50:50 +0900

    Update index.php

--- rank-web/index.php
+++ rank-web/index.php
@@ -5,7 +5,7 @@
 <?php
 	
 $config = parse_ini_file('app.ini'); 
-$conn = mysqli_connect($config->DB_NAME,$config->DB_USR,$config->DB_PASSWORD,$config->DB_DATABASE);
+$conn = mysqli_connect($config['DB_NAME'],$config['DB_USER'],$config['DB_PASSWORD'],$config['DB_DATABASE']);
 	
 
 $dbmsg = '';
