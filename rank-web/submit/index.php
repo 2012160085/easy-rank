f6dbfa30613e340baa56676d8400235f10e0ebcf
2012160085 <70303094+2012160085@users.noreply.github.com>
2020-09-07 13:51:49 +0900

    Update index.php

--- rank-web/submit/index.php
+++ rank-web/submit/index.php
@@ -1,1 +1,9 @@
-<?php ?>
+<?php
+$config = parse_ini_file('app.ini'); 
+$conn = mysqli_connect($config['DB_NAME'],$config['DB_USER'],$config['DB_PASSWORD'],$config['DB_DATABASE']);
+if($conn){
+  echo 'GOOD';
+else{
+  echo 'BAD';
+}
+?>
